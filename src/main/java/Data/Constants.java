f1ebf459faa311d2256ba9c2ab07bb5d30e3e50b
user1 <lashaxumara@gmail.com>
2023-06-16 23:58:48 +0400

    add RestAssured-homework-basics

---
+++ src/main/java/Data/Constants.java
@@ -0,0 +1,9 @@
+package Data;
+
+public class Constants {
+    public static String  countryUSA = "USA";
+    public static String countryHun = "Hungary";
+
+    public static int index1 = 1;
+    public static int index5 = 5;
+}
